8d355965a2070f92bb8d9dc03ca18a8f12344189
Dominik Strutz <kaaeveth@bofloos.de>
2025-08-20 12:53:42 +0200

    refac: Use compile-time borrow checking
    - Return mut reference of WindowCreationSettings to borrow check at
    compile-time

--- src/ui/window/window_creation_settings.rs
+++ src/ui/window/window_creation_settings.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, mem::MaybeUninit, sync::Once, thread::{self}};
+use std::{mem::MaybeUninit, sync::Once, thread::{self}};
 
 use i_slint_backend_winit::winit::window::WindowAttributes;
 
@@ -6,7 +6,7 @@ use i_slint_backend_winit::winit::window::WindowAttributes;
 /// ### NOTE
 /// **This method nor the [WindowCreationSettings] are thread-safe.
 /// Calling this method from any thread but the main thread will panic!**
-pub fn get_window_creation_settings() -> &'static WindowCreationSettings {
+pub fn get_window_creation_settings() -> &'static mut WindowCreationSettings {
     static mut WINDOW_SETTINGS: MaybeUninit<WindowCreationSettings> = MaybeUninit::uninit();
     static INIT: Once = Once::new();
 
@@ -17,18 +17,13 @@ pub fn get_window_creation_settings() -> &'static WindowCreationSettings {
     #[allow(static_mut_refs)]
     unsafe {
         INIT.call_once(|| {
-            let settings = WindowCreationSettings::new();
-            WINDOW_SETTINGS.write(settings);
+            WINDOW_SETTINGS.write(WindowCreationSettings::new());
         });
-        WINDOW_SETTINGS.assume_init_ref()
+        WINDOW_SETTINGS.assume_init_mut()
     }
 }
 
 pub struct WindowCreationSettings {
-    inner: RefCell<InnerCreationSettings>
-}
-
-struct InnerCreationSettings {
     default_settings: WindowAttributes,
     current_settings: WindowAttributes
 }
@@ -39,25 +34,22 @@ impl WindowCreationSettings {
             .with_visible(false)
             .with_transparent(true);
         Self {
-            inner: RefCell::new(InnerCreationSettings {
-                default_settings: attr.clone(),
-                current_settings: attr
-            })
+            default_settings: attr.clone(),
+            current_settings: attr
         }
     }
 
-    pub fn change(&self, change: impl Fn(WindowAttributes) -> WindowAttributes + 'static) -> SettingsChangedGuard {
-        let mut attr = self.inner.borrow_mut();
-        let new_attr = change(attr.default_settings.clone());
+    pub fn change(&mut self, change: impl Fn(WindowAttributes) -> WindowAttributes + 'static) -> SettingsChangedGuard {
+        let new_attr = change(self.default_settings.clone());
         let guard = SettingsChangedGuard { 
-            old_settings: Some(attr.current_settings.clone())
+            old_settings: Some(self.current_settings.clone())
         };
-        (*attr).current_settings = new_attr;
+        self.current_settings = new_attr;
         guard
     }
 
     pub fn get_settings(&self) -> WindowAttributes {
-        self.inner.borrow().current_settings.clone()
+        self.current_settings.clone()
     }
 }
 
@@ -70,10 +62,6 @@ pub struct SettingsChangedGuard {
 
 impl Drop for SettingsChangedGuard {
     fn drop(&mut self) {
-        // Borrowing may only fail if the guard is being dropped inside of a WindowCreationSettings::change
-        // callback, which should not happen anyway.
-        if let Ok(mut inner) = get_window_creation_settings().inner.try_borrow_mut() {
-            inner.current_settings = self.old_settings.take().unwrap();
-        }
+        get_window_creation_settings().current_settings = self.old_settings.take().unwrap();
     }
 }
