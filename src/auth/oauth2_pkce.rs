9fd69ee914b9075135c1a2f08c9c68c277d0c9b7
Dominik Strutz <kaaeveth@bofloos.de>
2025-08-23 14:03:29 +0200

    fix: Properly receive the OAuth auth code
    - Receive auth code without deadlocking

--- src/auth/oauth2_pkce.rs
+++ src/auth/oauth2_pkce.rs
@@ -1,15 +1,14 @@
-use std::{collections::HashMap, fmt::Display, str::FromStr, sync::Arc, time::Duration};
+use std::{collections::HashMap, fmt::Display, net::{Ipv4Addr, SocketAddrV4}, sync::Arc, time::Duration};
 
 use async_trait::async_trait;
 use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine as _};
 use rand::{rngs::StdRng, Rng, SeedableRng};
 use reqwest::{Client, Url};
 use sha2::Digest;
-use tokio::sync::mpsc::channel;
+use tokio::sync::{mpsc::channel};
 use tokio_util::sync::CancellationToken;
 use anyhow::{bail, Result, anyhow};
 use warp::{http::Response, Filter};
-use std::net::SocketAddr;
 
 use crate::{auth::{AuthResult, ServiceAuthenticator, TokenResult}, util::{build_query_string, open_link}};
 
@@ -69,57 +68,69 @@ impl OAuth2PKCEAuthenticator {
         URL_SAFE_NO_PAD.encode(sha2::Sha256::digest(&verifier))
     }
 
-    async fn receive_auth_code(&self, state: &str, cancel_token: CancellationToken) -> Result<String> {
-        let bind_host = self.redirect_url.authority();
-        let bind_path = self.redirect_url.path();
-        let state = Arc::new(state.to_owned());
-
+    async fn receive_auth_code(&self, state: String, cancel_token: CancellationToken) -> Result<String> {
         // Channel for propagating the auth codes and errors
         let (tx, mut rx) = channel::<Result<String>>(1);
 
-        let kill_server_token = cancel_token.child_token();
         let auth_res_handler = warp::get()
-            .and(warp::path(bind_path.to_owned()))
+            .and(warp::path::end())
             .and(warp::query::<QueryParams>())
-            .map({
-                let kill_server_token = kill_server_token.clone();
+            .then({
+                let state = Arc::new(state);
                 move |mut params: QueryParams| {
-                    let res = if params.remove("state").unwrap_or_default() != *state {
-                        "state missing or invalid".to_owned()
-                    } else if let Some(error) = params.remove("error") {
-                        error.to_owned()
-                    } else {
-                        if let Some(code) = params.remove("code") {
-                            let _ = tx.send(Ok(code));
-                            kill_server_token.cancel();
-                            return Response::builder().status(200).body("Success".into()).unwrap();
-                        }
-                        "'code' or 'error' missing".into()
-                    };
-                    let _ = tx.send(Err(anyhow!(res.clone())));
-                    kill_server_token.cancel();
-                    return Response::builder().status(500).body(res).unwrap();
+                    let tx = tx.clone();
+                    let state = state.clone();
+                    async move {
+                        let res = if params.remove("state").unwrap_or_default() != *state {
+                            "state missing or invalid".to_owned()
+                        } else if let Some(error) = params.remove("error") {
+                            error.to_owned()
+                        } else {
+                            if let Some(code) = params.remove("code") {
+                                let _ = tx.send(Ok(code)).await;
+                                return Response::builder().status(200).body("Success".into()).unwrap();
+                            }
+                            "'code' or 'error' missing".into()
+                        };
+                        let _ = tx.send(Err(anyhow!(res.clone())));
+                        return Response::builder().status(500).body(res).unwrap();
+                    }
                 }
             });
         
-        warp::serve(auth_res_handler)
-            .bind(SocketAddr::from_str(bind_host)?)
-            .await
-            .graceful(async move {
-                kill_server_token.cancelled().await
-            })
-            .run().await;
+        let kill_server_token = cancel_token.child_token();
+        let server_task = tokio::spawn({
+            let kill_server_token = kill_server_token.clone();
+            let port = self.redirect_url.port().unwrap_or(80);
+            async move {
+                let socket_addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port);
+                warp::serve(auth_res_handler)
+                    .bind(socket_addr)
+                    .await
+                    .graceful({
+                        async move {
+                            kill_server_token.cancelled().await
+                        }
+                    })
+                    .run()
+                    .await;
+            }
+        });
 
         tokio::select! {
             biased;
-            _ = cancel_token.cancelled() => {
-                bail!("Auth cancelled");
-            },
             res = rx.recv() => {
+                kill_server_token.cancel(); // Shutdown HTTP server
                 return match res {
                     Some(res) => res,
                     None => Err(anyhow!("BUG: Receiver closed before response was received"))
                 };
+            },
+            _ = cancel_token.cancelled() => {
+                bail!("Auth cancelled");
+            },
+            _ = server_task => {
+                bail!("BUG: HTTP Server shutdown unexpectedly");
             }
         }
     }
@@ -162,7 +173,7 @@ impl ServiceAuthenticator for OAuth2PKCEAuthenticator {
         auth_url.set_query(Some(&query));
         open_link(&auth_url);
 
-        let auth_code = self.receive_auth_code(&state, cancel.clone()).await?;
+        let auth_code = self.receive_auth_code(state, cancel.clone()).await?;
 
         // Request access token
         let body: HashMap<&str, &str> = [
@@ -184,5 +195,58 @@ impl ServiceAuthenticator for OAuth2PKCEAuthenticator {
         ].into_iter().collect();
         let res = self.send_token_req(&body).await?;
         Ok(AuthResult::Token(res))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use anyhow::{ensure, Context};
+    use tokio::sync::oneshot::channel;
+
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+    async fn receive_correct_auth_code() -> Result<()> {
+        let correct_auth_code = "6969";
+        let redirect_url =  Url::parse("http://localhost:14588")?;
+        let authenticator = OAuth2PKCEAuthenticator::new(
+            "c1337",
+            "user-read-private user-read-email",
+            Url::parse("https://accounts.spotify.com/")?,
+            redirect_url.clone(),
+            CodeChallengeMethod::Sha256
+        );
+        let http_client = reqwest::Client::new();
+        let state = authenticator.generate_verifier(CODE_VERIFIER_LEN);
+        let cancel_token = CancellationToken::new();
+        let (tx, rx) = channel::<Result<String>>();
+
+        let auth_task = tokio::spawn({
+            let state = state.clone();
+            let cancel_token = cancel_token.clone();
+            async move {
+                let res = authenticator.receive_auth_code(state, cancel_token).await;
+                let _ = tx.send(res);
+            }
+        });
+
+        let send_auth_code_res = http_client
+            .get(redirect_url)
+            .query(&[("code", correct_auth_code), ("state", &state)])
+            .timeout(Duration::from_secs(3))
+            .send()
+            .await.context("Timeout sending auth code")?
+            .error_for_status().context("Error sending auth code")?;
+        ensure!(send_auth_code_res.text().await?.as_str() == "Success", "Wrong response body received");
+
+        let received_auth_code = rx
+            .await
+            .context("Error receiving auth code")?
+            .context("Received error instead of auth code")?;
+        ensure!(received_auth_code == correct_auth_code, "Wrong auth code received");
+        
+        auth_task.await?;
+
+        Ok(())
     }
 }
